69bcb9b392d64668588c3cfbba32dd7912573989
Nicolas <nicolas.deriemacker@hotmail.fr>
2023-07-24 12:34:04 +0200

    ✨ F01: LOGIN > Add title and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,8 @@
-import { useState } from 'react';
+import LoginPage from './components/pages/LoginPage';
 const App = () => {
-	const handleSubmit = event => {
-		event.preventDefault();
-		alert(`Bonjour ${name} `);
-		setName('');
-	};
-	const [name, setName] = useState('');
-	const handleName = event => {
-		setName(event.target.value);
-	};
 	return (
 		<>
-			<h1>Bienvenue chez nous !</h1>
-			<h3>Connectez-vous</h3>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="Entrez votre prénom"
-					id="name"
-					name="email"
-					value={name}
-					onChange={handleName}
-					required
-				/>
-				<button>Accédez à votre espace</button>
-			</form>
+			<LoginPage />
 		</>
 	);
 };
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
 const LoginPage = () => {
-	return <div>LoginPage</div>;
+	const handleSubmit = event => {
+		event.preventDefault();
+		alert(`Bonjour ${name} `);
+		setName('');
+	};
+	const [name, setName] = useState('');
+	const handleName = event => {
+		setName(event.target.value);
+	};
+
+	return (
+		<div>
+			<h1>Bienvenue chez nous !</h1>
+			<h2>Connectez-vous</h2>
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Entrez votre prénom"
+					id="name"
+					name="email"
+					value={name}
+					onChange={handleName}
+					required
+				/>
+				<button>Accédez à votre espace</button>
+			</form>
+		</div>
+	);
 };
 
 export default LoginPage;

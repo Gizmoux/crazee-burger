2368e407d8f3d2e013bf50aba8289de9e51ae33d
Nicolas <nicolas.deriemacker@hotmail.fr>
2023-07-25 14:33:24 +0200

    ✨ F02: LOGIN > add redirection from/to errorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
-	return <div>ErrorPage</div>;
+	return (
+		<div>
+			<h1>Error Page...</h1>
+			<Link>
+				<button>Retourner vers la page d'accueil</button>
+			</Link>
+		</div>
+	);
 };
 
 export default ErrorPage;
